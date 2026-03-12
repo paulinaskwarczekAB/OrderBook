085dc6a58ee256415dcd09bcd933d0b8322879ea
Paulina Skwarczek <pskwarczek@albanybeck.com>
2026-03-12 10:39:09 +0100

    update

--- OrderService/src/main/java/com/trade/service/MatchingService.java
+++ OrderService/src/main/java/com/trade/service/MatchingService.java
@@ -35,21 +35,24 @@ public class MatchingService {
         List<Order> buys = orderRepository.getOrdersByInstrument(instrument).stream()
                 .filter(o -> o.getSide() == OrderSide.BUY)
                 .filter(o -> o.getStatus() == OrderStatus.PENDING || o.getStatus() == OrderStatus.PARTIALLY_FILLED)
-                .sorted(Comparator.comparing(Order::getPrice).reversed().thenComparing(Order::getCreatedAt))
+                .sorted(Comparator.comparing(Order::getPrice, Comparator.nullsLast(Comparator.naturalOrder()))
+                        .reversed()
+                        .thenComparing(Comparator.comparing(Order::getCreatedAt, Comparator.nullsLast(Comparator.naturalOrder()))))
                 .toList();
 
         List<Order> sells = orderRepository.getOrdersByInstrument(instrument).stream()
                 .filter(o -> o.getSide() == OrderSide.SELL)
                 .filter(o -> o.getStatus() == OrderStatus.PENDING || o.getStatus() == OrderStatus.PARTIALLY_FILLED)
-                .sorted(Comparator.comparing(Order::getPrice).reversed().thenComparing(Order::getCreatedAt))
+                .sorted(Comparator.comparing(Order::getPrice, Comparator.nullsLast(Comparator.naturalOrder()))
+                        .reversed()
+                        .thenComparing(Comparator.comparing(Order::getCreatedAt, Comparator.nullsLast(Comparator.naturalOrder()))))
                 .toList();
 
         for (Order buy : buys) {
             for (Order sell : sells) {
                 if(buy.getRemainingQuantity() == 0) break;
                 if(sell.getRemainingQuantity() == 0) continue;
-                if(buy.getPrice() >=sell.getPrice())
-                {
+                if(buy.getPrice() >= sell.getPrice()) {
                     doTransaction(buy, sell);
                 }
             }
@@ -69,8 +72,7 @@ public class MatchingService {
 
     private void actualiseOrder(Order order, int quantity) {
         order.setFilledQuantity(order.getFilledQuantity() + quantity);
-        if(order.getRemainingQuantity() == 0)
-        {
+        if(order.getRemainingQuantity() == 0) {
             order.setStatus(OrderStatus.FILLED);
         } else {
             order.setStatus(OrderStatus.PARTIALLY_FILLED);
