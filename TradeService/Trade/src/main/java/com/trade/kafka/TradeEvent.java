7b23dd4c93eacac819f590037230f1a987067cf3
Paulina Skwarczek <pskwarczek@albanybeck.com>
2026-03-12 10:49:47 +0100

    update

--- TradeService/Trade/src/main/java/com/trade/kafka/TradeEvent.java
+++ TradeService/Trade/src/main/java/com/trade/kafka/TradeEvent.java
@@ -1,12 +1,14 @@
 package com.trade.kafka;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class TradeEvent {
     private Long buyOrderId;
     private Long sellOrderId;
